25e129c738314ff1fccd2d6328bcf96be678fa6d
cotara <nurruslan@yandex.ru>
2022-10-16 14:00:55 +0700

    Попытка исправления в КЦ. Принято решение изменить концепцию с шаровым клапаном на 5 фиксированных позиций.

--- Olimex H103/Nextion/myNextion.h
+++ Olimex H103/Nextion/myNextion.h
@@ -9,8 +9,8 @@
 #define LED2_OFF GPIO_SetBits(GPIOE,GPIO_Pin_5)                                  //Управление светодиодом
 #define LED2_ON GPIO_ResetBits(GPIOE,GPIO_Pin_5)
 
-#define SHAR_STOP GPIO_SetBits(GPIOE,GPIO_Pin_7)                                //Стопор шара
-#define SHAR_START GPIO_ResetBits(GPIOE,GPIO_Pin_7)
+#define SHAR_STOP GPIO_SetBits(GPIOE,GPIO_Pin_6)                                //Стопор шара
+#define SHAR_START GPIO_ResetBits(GPIOE,GPIO_Pin_6)
 
 #define SHAR_OPEN GPIO_SetBits(GPIOD,GPIO_Pin_0)                                //ШАР
 #define SHAR_CLOSE GPIO_ResetBits(GPIOD,GPIO_Pin_0)
--- Olimex H103/Nextion/user_GPIO.c
+++ Olimex H103/Nextion/user_GPIO.c
@@ -15,7 +15,7 @@ void GPIO_init(void)
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;	
   GPIO_Init(GPIOA, &GPIO_InitStructure);
   
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 |GPIO_Pin_5| GPIO_Pin_0;             //реле, рвущее питание на ШАР
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 |GPIO_Pin_5| GPIO_Pin_0;             //реле, рвущее питание на ШАР
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;	
   GPIO_Init(GPIOE, &GPIO_InitStructure);
